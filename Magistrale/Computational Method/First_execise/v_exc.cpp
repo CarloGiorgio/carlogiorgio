b23d6467d609c9305a5446d2bd51b3fe31c9f174
CarloGiorgio <carlo.giorgetti98@gmail.com>
2020-10-23 22:54:19 +0200

    New exercise

---
+++ Magistrale/Computational Method/First_execise/v_exc.cpp
@@ -0,0 +1,35 @@
+#include<cstdlib>
+#include <iostream>
+using namespace std;
+template <class ntype>
+
+class Particle{
+  double v[3]={};
+public:
+  void set(double *val){
+    for(int i =0;i<3;i++){
+      v[i]=val[i];
+    }
+  }
+
+  void show(){
+    cout <<"Vector position"<<endl;
+    for(int i =0;i<3;i++){
+      cout <<v[i]<<'\n';
+    }
+  }
+};
+
+class Molecule: public Particle{
+
+public:
+  void
+}
+
+
+int main(){
+  Particle <double> p;
+  double x[3]={1.,1.,1.};
+  p.set(x);
+  p.show();
+}
